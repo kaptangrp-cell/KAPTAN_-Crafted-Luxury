502b02ebc8fabfc91e969358269e70d467a3609c
pakistanisinmarburg-jpg <pakistanisinmarburg@gmail.com>
2026-06-19 12:18:43 +0200

    checkout move

--- src/routes/checkout.tsx
+++ src/routes/checkout.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/_authenticated/checkout')({
+export const Route = createFileRoute('/checkout')({
   component: RouteComponent,
 })
 
